b108ed5dfdd099d3f659f2a4e18a3cf64b006785
pro-minhaj <parsonal322532@gmail.com>
2024-06-08 17:54:46 +0600

    Implement Create testimonials Modal using mongoose Schema

---
+++ modals/testimonials-modal.js
@@ -0,0 +1,28 @@
+import mongoose, { Schema } from 'mongoose';
+
+const testimonialSchema = new Schema(
+    {
+        content: {
+            type: String,
+            required: true
+        },
+        rating: {
+            type: Number,
+            required: true
+        },
+        courseId: {
+            type: Schema.Types.ObjectId,
+            ref: 'Course',
+            required: true
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true
+        }
+    },
+    { timestamps: true }
+);
+
+export const Testimonial =
+    mongoose.models.Testimonial ?? mongoose.model('Testimonial', testimonialSchema);
