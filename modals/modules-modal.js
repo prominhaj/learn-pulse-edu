c7ddaec4e4921f96b4277712587e7dde0c723f9a
pro-minhaj <parsonal322532@gmail.com>
2024-06-08 17:48:18 +0600

    Implement Modules Modal Create

---
+++ modals/modules-modal.js
@@ -0,0 +1,36 @@
+import mongoose, { Schema } from 'mongoose';
+
+const moduleSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        status: {
+            type: String,
+            required: true
+        },
+        slug: {
+            type: String,
+            required: true
+        },
+        course: {
+            type: Schema.Types.ObjectId,
+            ref: 'Course',
+            required: true
+        },
+        lessonIds: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Lesson'
+            }
+        ]
+    },
+    { timestamps: true }
+);
+
+export const Module = mongoose.models.Module ?? mongoose.model('Module', moduleSchema);
