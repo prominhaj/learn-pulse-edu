845deb99a07a998094f628c0312cbe8707b643ba
pro-minhaj <parsonal322532@gmail.com>
2024-06-08 17:52:10 +0600

    Implement Lessons Modal Create

---
+++ modals/lessons-modal.js
@@ -0,0 +1,40 @@
+import mongoose, { Schema } from 'mongoose';
+
+const lessonSchema = new Schema(
+    {
+        title: {
+            type: String,
+            required: true
+        },
+        description: {
+            type: String,
+            required: true
+        },
+        duration: {
+            type: String,
+            required: true
+        },
+        video_url: {
+            type: String,
+            required: true
+        },
+        published: {
+            type: Boolean,
+            required: true,
+            default: false
+        },
+        slug: {
+            type: String,
+            require: false
+        },
+        access: {
+            type: String,
+            required: true,
+            default: 'public',
+            enum: ['public', 'private']
+        }
+    },
+    { timestamps: true }
+);
+
+export const Lesson = mongoose.models.Lesson ?? mongoose.model('Lesson', lessonSchema);
