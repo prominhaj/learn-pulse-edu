a52a6402fdb33f5926ad36b447e54e6b75431adb
pro-minhaj <parsonal322532@gmail.com>
2024-06-15 22:49:16 +0600

    Implement Dashboard Home Page BarChart Component Create And Show by Home Page

--- app/dashboard/page.js
+++ app/dashboard/page.js
@@ -1,44 +1,48 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { formatPrice } from "@/lib/formatPrice";
+import BarChart from '@/components/globals/BarChart/BarChart';
+import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
+import { formatPrice } from '@/lib/formatPrice';
 formatPrice;
 
 const DashboardPage = async () => {
-  return (
-    <div className="p-6">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4 mb-4">
-        {/* total courses */}
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Courses</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">15</div>
-          </CardContent>
-        </Card>
-        {/* total enrollments */}
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">
-              Total Enrollments
-            </CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">1000</div>
-          </CardContent>
-        </Card>
-        {/* total revinue */}
-        <Card>
-          <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
-            <CardTitle className="text-sm font-medium">Total Revenue</CardTitle>
-          </CardHeader>
-          <CardContent>
-            <div className="text-2xl font-bold">{formatPrice(12000)}</div>
-          </CardContent>
-        </Card>
-      </div>
-      {/*  */}
-    </div>
-  );
+    return (
+        <div className='p-6'>
+            <div className='grid grid-cols-1 gap-4 mb-4 md:grid-cols-2 lg:grid-cols-3'>
+                {/* total courses */}
+                <Card>
+                    <CardHeader className='flex flex-row items-center justify-between pb-2 space-y-0'>
+                        <CardTitle className='text-sm font-medium'>Total Courses</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className='text-2xl font-bold'>15</div>
+                    </CardContent>
+                </Card>
+                {/* total enrollments */}
+                <Card>
+                    <CardHeader className='flex flex-row items-center justify-between pb-2 space-y-0'>
+                        <CardTitle className='text-sm font-medium'>Total Enrollments</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className='text-2xl font-bold'>1000</div>
+                    </CardContent>
+                </Card>
+                {/* total Revinue */}
+                <Card>
+                    <CardHeader className='flex flex-row items-center justify-between pb-2 space-y-0'>
+                        <CardTitle className='text-sm font-medium'>Total Revenue</CardTitle>
+                    </CardHeader>
+                    <CardContent>
+                        <div className='text-2xl font-bold'>{formatPrice(12000)}</div>
+                    </CardContent>
+                </Card>
+            </div>
+            <div className='grid grid-cols-3 gap-5 pt-5'>
+                <Card className='col-span-2 p-3'>
+                    <BarChart />
+                </Card>
+                <Card className='p-3'>Recent Sale</Card>
+            </div>
+        </div>
+    );
 };
 
 export default DashboardPage;
