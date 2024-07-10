6cd8f433dd2a2bffc74761066ec073b0a6d4a67e
Md Minhaj <139013851+pro-minhaj@users.noreply.github.com>
2024-07-10 23:30:38 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,7 @@ import withPlaiceholder from '@plaiceholder/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    output: "export",
     images: {
         domains: ['res.cloudinary.com'],
         remotePatterns: [
