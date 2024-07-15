ed6a1a07c81cc25ca93293bb258be920382605ba
Md Minhaj <139013851+pro-minhaj@users.noreply.github.com>
2024-07-15 19:31:52 +0600

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -2,9 +2,6 @@ import withPlaiceholder from '@plaiceholder/next';
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-eslint:{
-ignoreDuringBuilds: true
-}
     images: {
         domains: ['res.cloudinary.com'],
         remotePatterns: [
